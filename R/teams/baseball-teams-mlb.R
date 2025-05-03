ac259579b38eacc020f06443c4969dd040d00945
Cullen Champagne <cullenchampagne@icloud.com>
2025-05-03 18:27:11 -0500

    refactored mlb teams

--- R/teams/baseball-teams-mlb.R
+++ R/teams/baseball-teams-mlb.R
@@ -25,31 +25,31 @@ source("R/util-data-report.R")
 # Utilties for updating markdown
 source("R/util-markdown.R")
 
-library(rvest, quietly = TRUE, warn.conflicts = FALSE) # Filter through and parse html objects 
+library(rvest, quietly = TRUE, warn.conflicts = FALSE) # Filter through and parse html objects
 library(tidyr, quietly = TRUE, warn.conflicts = FALSE) # Unest list attributed to columns in dataframe
 library(dplyr, quietly = TRUE, warn.conflicts = FALSE) # Mutation / Management of dataframes
 library(yaml, quietly = TRUE, warn.conflicts = FALSE) # Load yaml configiugration into program
 
 # Hardcoded list of mlb team twitter acounts because couldnt find online
-MLB_TWITTER_ACCOUNTS <- yaml::read_yaml("data/mutations/baseball_mlb_mutations.yaml")$TWITTER_ACCOUNTS
+mlb_twitter_acounts <- yaml::read_yaml("data/mutations/baseball_mlb_mutations.yaml")$TWITTER_ACCOUNTS
 # Hardcoded list of mbl team websites because couldnt find online
-MLB_WEBSITES <- yaml::read_yaml("data/mutations/baseball_mlb_mutations.yaml")$WEBSITES
+mlb_websites <- yaml::read_yaml("data/mutations/baseball_mlb_mutations.yaml")$WEBSITES
 # Read configuration from configs directory
-CONFIG <- yaml::read_yaml("configs/baseball_mlb.yaml")
+config <- yaml::read_yaml("configs/baseball_mlb.yaml")
 # File to hold formated data
-ALL_TEAMS_FILE <- "data/processed/baseball-teams-mlb.csv"  
+all_teams_file <- "data/processed/baseball-teams-mlb.csv"
 
 #' MLB Teams
 #'
-#' Retrieves MLB team data from ESPN's API and supplements it with additional 
-#' information scraped from the wiki team pages. The combined data is processed into a structured 
+#' Retrieves MLB team data from ESPN's API and supplements it with additional
+#' information scraped from the wiki team pages. The combined data is processed into a structured
 #' dataframe and saved to a CSV file.
-#' 
+#'
 #' @source https://site.api.espn.com/
 #' @source https://en.wikipedia.org/wiki/
 #'
-#' @param VERBOSE Logical indicating whether to print progress messages (default: TRUE)
-#' 
+#' @param verbose Logical indicating whether to print progress messages (default: TRUE)
+#'
 #' @return A dataframe containing the following information for each NBA team
 #'  id [string] - A generated unique identifier for each team
 #'  espn_id [int] - id used be espn to identify team
@@ -57,25 +57,25 @@ ALL_TEAMS_FILE <- "data/processed/baseball-teams-mlb.csv"
 #'  abv [string] - Abreviation of team name (ex. DEN)
 #'  full_name [string] - Full name of team (ex. Denver Nuggets)
 #'  short_name [string] - Short name of team (ex. Nuggets)
+#'  league [string] - League team is associated with (ex. National League)
+#'  division [string] - Division team is associated with (ex. Central Division)
 #'  primary [string] - Primary color of team uniforms in Hex format
 #'  secondary [string] - Secondary color of team uniforms in Hex format
 #'  logo [string] - Link to logo image from ESPN
-#'  league [string] - League team is associated with (ex. National League)
-#'  division [string] - Division team is associated with (ex. Central Division)
 #'  twitter [string] - Twitter handle of team starting with '@'
 #'  webiste [string] - Website url for team
 #'  general_manager [string] - Current general manager of team
-#'  manager [string] - Current manager of team 
-#'  venue [string] - Current venue where team plays 
+#'  manager [string] - Current manager of team
+#'  venue [string] - Current venue where team plays
 #'
-get_formated_data <- function(VERBOSE = TRUE) {
+get_formated_data <- function(verbose = TRUE) {
 
     # Processes raw ESPN team JSON data into structured dataframe
-    # 
+    #
     #' @param team List object containing raw team data from ESPN API
-    #' 
+    #'
     #' @return A dataframe containing all information in the espn json structure
-    #' 
+    #'
     extract_team <- function(team) {
         # Catalog of base fields to extract from data
         base_fields <- c("id", "uid", "slug", "abbreviation", "displayName",
@@ -94,12 +94,11 @@ get_formated_data <- function(VERBOSE = TRUE) {
         team_links <- setNames(sapply(team$links, function(x) x$href), make.names(sapply(team$links, function(x) x$text)))
         # Bind base fields / logo with avalaible links
         espn_team_df <- cbind(base_fields_df, t(team_links))
-        return(espn_team_df)
     }
 
     # Download NFL team data
-    all_mlb_teams <- download_fromJSON(CONFIG$LINKS$ESPN_TEAMS, simplifyDataFrame = FALSE)
-    if (VERBOSE) cat(paste0("\n\033[32mDownloading MLB Teams: ", CONFIG$LINKS$ESPN_TEAMS, "\033[0m"))
+    all_mlb_teams <- download_fromJSON(config$LINKS$ESPN_TEAMS, simplifyDataFrame = FALSE)
+    if (verbose) cat(paste0("\n\033[32mDownloading MLB Teams: ", config$LINKS$ESPN_TEAMS, "\033[0m"))
     # Extract all teams from ESPN json structure
     espn_teams_list <- lapply(all_mlb_teams$sports[[1]]$leagues[[1]]$teams, function(x) x$team)
     # Run extract teams function on all teams in data
@@ -109,33 +108,33 @@ get_formated_data <- function(VERBOSE = TRUE) {
 
     # Init blank data frame for team details
     mlb_team_details <- data.frame()
-    # Loop through team detail webpages and select data 
-    if (VERBOSE) cat(paste0("\n\033[32mDownloading MLB Team Information: https://en.wikipedia.org/wiki/...\n\033[0m"))
-    for (url in CONFIG$LINKS$TEAM_DETAILS) {
+    # Loop through team detail webpages and select data
+    if (verbose) cat(paste0("\n\033[32mDownloading MLB Team Information: https://en.wikipedia.org/wiki/...\n\033[0m"))
+    for (url in config$LINKS$TEAM_DETAILS) {
         # Download page content from url
         page_content <- download_fromHTML(url)
         # Get name from xpath
-        name <- page_content %>% rvest::html_element(xpath = CONFIG$ATTRIBUTES$TEAM_DETAILS$NAME) %>% rvest::html_text(trim = TRUE)
+        name <- page_content %>% rvest::html_element(xpath = config$ATTRIBUTES$TEAM_DETAILS$NAME) %>% rvest::html_text(trim = TRUE)
         # Get arena from xpath
-        venue_text <- page_content %>% rvest::html_element(xpath = CONFIG$ATTRIBUTES$TEAM_DETAILS$ARENA) %>% rvest::html_text(trim = TRUE)
+        venue_text <- page_content %>% rvest::html_element(xpath = config$ATTRIBUTES$TEAM_DETAILS$ARENA) %>% rvest::html_text(trim = TRUE)
         venue <- venue_text %>% gsub("(\\w+)\\s+(\\w+).*", "\\1 \\2", .)
         # Get head coach data from xpath
-        coach_text <- page_content %>% rvest::html_element(xpath = CONFIG$ATTRIBUTES$TEAM_DETAILS$COACH) %>% rvest::html_text(trim = TRUE)
+        coach_text <- page_content %>% rvest::html_element(xpath = config$ATTRIBUTES$TEAM_DETAILS$COACH) %>% rvest::html_text(trim = TRUE)
         head_coach <- coach_text %>% gsub("(\\w+)\\s+(\\w+).*", "\\1 \\2", .)
         # Get head coach data from xpath
-        manager_text <- page_content %>% rvest::html_element(xpath = CONFIG$ATTRIBUTES$TEAM_DETAILS$MANAGER) %>% rvest::html_text(trim = TRUE)
+        manager_text <- page_content %>% rvest::html_element(xpath = config$ATTRIBUTES$TEAM_DETAILS$MANAGER) %>% rvest::html_text(trim = TRUE)
         manager <- manager_text %>% gsub("(\\w+)\\s+(\\w+).*", "\\1 \\2", .)
         # Get division data from xpath
-        division <- page_content %>% rvest::html_element(xpath = CONFIG$ATTRIBUTES$TEAM_DETAILS$DIVISION) %>% rvest::html_text(trim = TRUE)
+        division <- page_content %>% rvest::html_element(xpath = config$ATTRIBUTES$TEAM_DETAILS$DIVISION) %>% rvest::html_text(trim = TRUE)
         # Get conference data from xpath
-        conference <- page_content %>% rvest::html_element(xpath = CONFIG$ATTRIBUTES$TEAM_DETAILS$CONFERENCE) %>% rvest::html_text(trim = TRUE)
+        league <- page_content %>% rvest::html_element(xpath = config$ATTRIBUTES$TEAM_DETAILS$CONFERENCE) %>% rvest::html_text(trim = TRUE)
         # Return dataframe with parsed information
         mlb_team_details <- rbind(mlb_team_details, data.frame(
             displayName = name,
-            conference = conference,
+            league = league,
             division = division,
-            twitter = paste0("@", MLB_TWITTER_ACCOUNTS[name]),
-            website = MLB_WEBSITES[[name]],
+            twitter = paste0("@", mlb_twitter_acounts[name]),
+            website = mlb_websites[[name]],
             manager = head_coach,
             general_manager = manager,
             venue = venue,
@@ -148,33 +147,36 @@ get_formated_data <- function(VERBOSE = TRUE) {
     all_mlb_teams <- all_mlb_teams %>% dplyr::left_join(mlb_team_details, by = "displayName") %>% dplyr::rename(espn_id = id)
 
     # Generate Unique team ids for each team
-    all_mlb_teams <- all_mlb_teams %>% dplyr::mutate(id = encode_id(paste0("B", espn_id), abbreviation)) %>% dplyr::filter(isActive == TRUE) %>%
+    all_mlb_teams <- all_mlb_teams %>%
+        dplyr::mutate(id = encode_id(paste0("B", espn_id), abbreviation)) %>%
+        dplyr::filter(isActive == TRUE) %>%
         # Remove uneeded columns
         dplyr::select(-c(uid, name, nickname, isActive, isAllStar, Clubhouse, Roster,
-        Statistics, Schedule, Tickets, Depth.Chart, slug, 'location.1')) %>%
+        Statistics, Schedule, Tickets, Depth.Chart, slug, "location.1")) %>%
         # Rename all columns to follow standard
-        dplyr::rename(full_name = displayName, short_name = shortDisplayName, primary = color, 
+        dplyr::rename(full_name = displayName, short_name = shortDisplayName, primary = color,
             secondary = alternateColor, abv = abbreviation) %>%
         # Capitalize all hex colors
         dplyr::mutate(primary = toupper(primary), secondary = toupper(secondary)) %>%
         # Add a stanard # before color values if isnt already there and value isnt NA
         dplyr::mutate(dplyr::across(c(primary, secondary), ~ ifelse(!is.na(.) & !str_starts(., "#"), paste0("#", .), .))) %>%
         # Create a type column and move after id
-        dplyr::mutate(type = "MLB") %>% dplyr::select(id, espn_id, type, dplyr::everything()) %>% dplyr::relocate(venue, .after = last_col()) 
+        dplyr::mutate(type = "MLB") %>%
+        dplyr::select(id, espn_id, type, abv, full_name, short_name, league, division, dplyr::everything()) %>%
+        dplyr::relocate(twitter, .after = last_col()) %>%
+        dplyr::relocate(website, .after = last_col()) %>%
+        dplyr::relocate(venue, .after = last_col())
 
     # Analyze missing data
     analyze_missing_data("MLB", all_mlb_teams)
     process_markdown_file("R/teams/baseball-teams-mlb.R", "R/teams/readme.md", nrow(all_mlb_teams))
 
-    if (VERBOSE) cat(paste0("\n\033[90mMLB Baseball Data Saved To: /", ALL_TEAMS_FILE, "\033[0m\n"))
+    if (verbose) cat(paste0("\n\033[90mMLB Baseball Data Saved To: /", all_teams_file, "\033[0m\n"))
     # Save any created name bindings to file
-    write.csv(all_mlb_teams, ALL_TEAMS_FILE, row.names = FALSE)
+    write.csv(all_mlb_teams, all_teams_file, row.names = FALSE)
     # Return formated data
     return(all_mlb_teams)
 }
 
 # If file is being run stand-alone, run function
 if (interactive()) get_formated_data()
-
-
-
